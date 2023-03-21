c769f829ef7c22dac3704effcfeddd849e0f8fdc
Boris Ostretsov <borisostretsov@gmail.com>
2023-03-22 02:44:49 +0300

    Add files via upload

--- JavaLab5/src/javalab5/JavaLab5.java
+++ JavaLab5/src/javalab5/JavaLab5.java
@@ -4,6 +4,7 @@
  */
 package javalab5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -12,28 +13,37 @@ import java.util.Scanner;
  */
 public class JavaLab5 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Для квадратного уравнения вида a*х^2 + b*x + c = 0 введите числа a, b и c каждое на отдельной строке:");
+    
+    private static void quadraticEquation(){
+        System.out.println("Для квадратного уравнения вида a*х^2 + b*x + c = 0 введите целые числа a, b и c, каждое на отдельной строке или через пробел:");
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = b*b-4*a*c;
-        if(d < 0){
-            System.out.println("Уравнение не имеет корней!");
+        try{
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            int d = b*b-4*a*c;
+            if(d < 0){
+                System.out.println("Уравнение не имеет корней!");
+            }
+            else if(d == 0){
+                System.out.println("Уравнение имеет один корень!");
+                System.out.println("X = "+(-b/(2*a)));
+            }
+            else{
+                System.out.println("Уравнение имеет два корня!");
+                System.out.println("X1 = "+(-b-Math.sqrt(d))/(2*a));
+                System.out.println("X2 = "+(-b+Math.sqrt(d))/(2*a));
+            }
         }
-        else if(d == 0){
-            System.out.println("Уравнение имеет один корень!");
-            System.out.println("X = "+(-b/(2*a)));
+        catch(InputMismatchException e){
+            System.out.println("Ошибка ввода! Недопустимое значение одной из переменных! Будьте внимательнее!");
+            quadraticEquation();
         }
-        else{
-            System.out.println("Уравнение имеет два корня!");
-            System.out.println("X1 = "+(-b-Math.sqrt(d))/(2*a));
-            System.out.println("X2 = "+(-b+Math.sqrt(d))/(2*a));
-        }
+    }
+    
+    public static void main(String[] args) {
+        quadraticEquation();
     }
     
 }
